166d253736c5a5973c9ebcd3e54d5e25c166183b
deanchristakos <dean@alum.mit.edu>
2021-05-26 23:20:00 -0400

    added gitignore, helloworld.c, and makefile

---
+++ helloworld.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  helloworld.c
+ *
+ *    Description: just a hello world file 
+ *
+ *        Version:  1.0
+ *        Created:  05/26/2021 23:14:37
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Constantine (Dean) Christakos (dkc), dean@christakos.com
+ *        Company:  Zounda Analytics, LLC
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("Hello, world!\n");
+}
+
